733c650f1b06900ea982f30c805653189d7ce174
JY <shutingz@naver.com>
2020-10-18 13:07:59 +0900

    POST Event JpaRepository add

---
+++ src/main/java/jy/learning/bootrestapi/events/EventRepository.java
@@ -0,0 +1,7 @@
+package jy.learning.bootrestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
